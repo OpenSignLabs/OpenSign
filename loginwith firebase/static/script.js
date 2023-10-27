484e8cd0c36341de79c3c874432dc2e646498ca6
Aditya Kumar <106132841+Aditya-567@users.noreply.github.com>
2023-10-27 21:34:08 +0530

    Add files via upload

---
+++ loginwith firebase/static/script.js
@@ -0,0 +1,11 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
