820d6378f9159221b732d15094b6b324f4930db7
prafull-opensignlabs <prafull.navkar@nxglabs.com>
2023-11-06 19:44:06 +0530

    fix: add loadmf route to load login flow of microapp, add pagenotfound microapp

---
+++ microfrontends/SignDocuments/src/Component/PageNotFound.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div className="flex items-center justify-center h-screen w-full bg-white rounded">
+      <div className="text-center">
+        <h1 className="text-[60px] lg:text-[120px] font-semibold text-black">
+          404
+        </h1>
+        <p className="text-[30px] lg:text-[50px] text-black">Page Not Found</p>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
