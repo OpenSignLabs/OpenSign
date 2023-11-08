5fa60d8135703efb4b2ebd0fd09c6520539765d5
RaktimaNXG <raktima.c@nxglabs.com>
2023-11-08 20:42:35 +0530

    solved overflow header on logiin page

--- microfrontends/SignDocuments/src/Component/component/header.js
+++ microfrontends/SignDocuments/src/Component/component/header.js
@@ -37,7 +37,7 @@ function Header({
 }) {
   const isMobile = window.innerWidth < 767;
   const navigate = useNavigate();
-
+  const isSigner = localStorage.getItem("isSigner");
   //for go to previous page
   function previousPage() {
     changePage(-1);
@@ -81,14 +81,16 @@ function Header({
 
   return (
     <div
-      style={{ paddingBottom: "5px", paddingTop: "5px" }}
+    style={{ padding: !isSigner && "5px 0px 5px 0px" }}
       className="mobileHead"
     >
       {isMobile && isShowHeader ? (
         <div
           id="navbar"
-          className="stickyHead"
-          style={{ width: window.innerWidth - 30 + "px" }}
+          className={isSigner ? "stickySignerHead" : "stickyHead"}
+          style={{
+            width: isSigner ? window.innerWidth : window.innerWidth - 30 + "px"
+          }}
         >
           <div className="preBtn2">
             <div
