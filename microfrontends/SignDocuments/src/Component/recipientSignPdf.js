74bdca5081365324428fd3f42dd0cd3aa3661720
Raktima <110812506+raktima-opensignlabs@users.noreply.github.com>
2023-11-14 15:29:00 +0530

    fix need your sign report document signature issue (#183)

--- microfrontends/SignDocuments/src/Component/recipientSignPdf.js
+++ microfrontends/SignDocuments/src/Component/recipientSignPdf.js
@@ -1083,6 +1083,7 @@ function EmbedPdfImage() {
               isShowHeader={true}
               currentSigner={true}
               decline={true}
+              alreadySign={pdfUrl ? true :false}
             />
 
             <RenderPdf
