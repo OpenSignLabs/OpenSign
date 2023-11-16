e16a18fabc1c3047caf1f63a2f9470e54b4e3dc5
prafull-opensignlabs <93375423+prafull-opensignlabs@users.noreply.github.com>
2023-11-16 11:02:38 +0530

    security improvement (#190)

---
+++ apps/OpenSignServer/cloud/parsefunction/getUserDetails.js
@@ -0,0 +1,12 @@
+async function getUserDetails(request) {
+  try {
+    const userQuery = new Parse.Query('contracts_Users');
+    userQuery.equalTo('Email', request.params.email);
+    const res = await userQuery.first({ useMasterKey: true });
+    return res;
+  } catch (err) {
+    console.log('Err ', err);
+    return err;
+  }
+}
+export default getUserDetails;
