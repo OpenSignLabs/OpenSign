93a891c8b680065522cd843089a944a041381192
prafull-opensignlabs <prafull.navkar@nxglabs.com>
2023-11-16 10:47:11 +0530

    feat: add migration code for contracts_users class

--- apps/OpenSignServer/index.js
+++ apps/OpenSignServer/index.js
@@ -17,10 +17,11 @@ import S3Adapter from 'parse-server-s3-adapter';
 import FSFilesAdapter from 'parse-server-fs-adapter';
 import AWS from 'aws-sdk';
 import { app as customRoute } from './cloud/customRoute/customApp.js';
+import { exec } from 'child_process';
 
 const spacesEndpoint = new AWS.Endpoint(process.env.DO_ENDPOINT);
 // console.log("configuration ", configuration);
-if (process.env.USE_LOCAL !== "TRUE") {
+if (process.env.USE_LOCAL !== 'TRUE') {
   const s3Options = {
     bucket: process.env.DO_SPACE, // globalConfig.S3FilesAdapter.bucket,
     baseUrl: process.env.DO_BASEURL,
@@ -36,7 +37,7 @@ if (process.env.USE_LOCAL !== "TRUE") {
   var fsAdapter = new S3Adapter(s3Options);
 } else {
   var fsAdapter = new FSFilesAdapter({
-    "filesSubDirectory": "files" // optional, defaults to ./files
+    filesSubDirectory: 'files', // optional, defaults to ./files
   });
 }
 
@@ -167,4 +168,22 @@ if (!process.env.TESTING) {
   });
   // This will enable the Live Query real-time server
   await ParseServer.createLiveQueryServer(httpServer);
+
+  const migrate =
+    'APPLICATION_ID=legadranaxn SERVER_URL=http://localhost:8080/app MASTER_KEY=XnAwPDRQQyMr npx parse-dbtool migrate';
+  // `APPLICATION_ID=${process.env.APP_ID} SERVER_URL=${process.env.SERVER_URL} MASTER_KEY=${process.env.MASTER_KEY} npx parse-dbtool migrate`;
+
+  exec(migrate, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return;
+    }
+
+    if (stderr) {
+      console.error(`Error: ${stderr}`);
+      return;
+    }
+
+    console.log(`Command output: ${stdout}`);
+  });
 }
