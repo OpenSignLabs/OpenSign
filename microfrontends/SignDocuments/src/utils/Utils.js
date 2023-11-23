818511edb08128b94478d5d1261f4d5b096278fd
Amol <amol@nxglabs.in>
2023-11-23 17:57:00 +0530

    chore: syntax issue

--- microfrontends/SignDocuments/src/utils/Utils.js
+++ microfrontends/SignDocuments/src/utils/Utils.js
@@ -331,7 +331,7 @@ export function urlValidator(url) {
   } catch (err) {
     return false;
   }
-export const modalAlign = () => {
+export function modalAlign() {
   let modalDialog = $('.modal-dialog').getBoundingClientRect();
   let mobileHead = $('.mobileHead').getBoundingClientRect()
   let modal = $('.modal-dialog');
