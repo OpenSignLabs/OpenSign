9c8d07d724a59a15d7a3fc4fd18be4afb03d20c2
prafull-opensignlabs <prafull.navkar@nxglabs.com>
2023-12-02 21:16:56 +0530

    created generateapitoken, getapitoken cloud function & generatetoken route

--- apps/OpenSignServer/cloud/main.js
+++ apps/OpenSignServer/cloud/main.js
@@ -17,6 +17,8 @@ import getUserDetails from './parsefunction/getUserDetails.js';
 import getDocument from './parsefunction/getDocument.js';
 import getDrive from './parsefunction/getDrive.js';
 import getReport from './parsefunction/getReport.js';
+import generateApiToken from './parsefunction/generateApiToken.js';
+import getapitoken from './parsefunction/getapitoken.js';
 
 Parse.Cloud.define('AddUserToRole', addUserToGroups);
 Parse.Cloud.define('UserGroups', getUserGroups);
@@ -35,5 +37,7 @@ Parse.Cloud.define('AuthLoginAsMail', AuthLoginAsMail);
 Parse.Cloud.define('getUserId', getUserId);
 Parse.Cloud.define('getUserDetails', getUserDetails);
 Parse.Cloud.define('getDocument', getDocument);
-Parse.Cloud.define('getDrive', getDrive)
-Parse.Cloud.define('getReport', getReport)
+Parse.Cloud.define('getDrive', getDrive);
+Parse.Cloud.define('getReport', getReport);
+Parse.Cloud.define('generateapitoken', generateApiToken);
+Parse.Cloud.define('getapitoken', getapitoken);
