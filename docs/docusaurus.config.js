b12eb87b384f9027146d35bcbc9262348eaced83
nxglabs <amol@nxglabs.in>
2023-12-18 20:34:20 +0530

    chore: setup github pages action to publish docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -20,8 +20,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'opensignlabs', // Usually your GitHub org/user name.
+  projectName: 'opensign', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
