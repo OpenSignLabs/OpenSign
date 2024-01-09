8b306acb8bd1859acb81fdde28ecbea0645d9403
RaktimaNXG <raktima.c@nxglabs.com>
2024-01-09 14:35:45 +0530

    fix: folder list display issue in move folder modal,add alert for delete document, change modal css

--- microfrontends/SignDocuments/src/premitives/SelectFolder.js
+++ microfrontends/SignDocuments/src/premitives/SelectFolder.js
@@ -10,7 +10,8 @@ const SelectFolder = ({
   onSuccess,
   folderCls,
   isOpenModal,
-  setIsOpenMoveModal
+  setIsOpenMoveModal,
+  setPdfData
 }) => {
   const [clickFolder, setClickFolder] = useState("");
   // const [selectFolder, setSelectedFolder] = useState({});
@@ -28,39 +29,14 @@ const SelectFolder = ({
       fetchFolder();
     }
   }, []);
+  //fetch folder data
   const fetchFolder = async (folderPtr) => {
     setIsLoader(true);
-
-    // try {
-    //   console.log("folder",folderCls,folderPtr)
-    //   const FolderQuery = new Parse.Query('contracts_Document');
-    //   console.log("folder",FolderQuery)
-    //   if (folderPtr) {
-    //     FolderQuery.equalTo("Folder", folderPtr);
-    //     FolderQuery.equalTo("Type", "Folder");
-    //   } else {
-    //     FolderQuery.doesNotExist("Folder");
-    //     FolderQuery.equalTo("Type", "Folder");
-    //   }
-    //   console.log("newres",FolderQuery)
-    //   const res = await FolderQuery.count();
-    //   console.log("res",res)
-    //   // if (res) {
-    //   //   const result = JSON.parse(JSON.stringify(res));
-    //   //   if (result) {
-    //   //     setFolderList(result);
-    //   //     setIsLoader(false);
-    //   //   }
-    //   //   setIsLoader(false);
-    //   // }
-    // } catch (error) {
-    //   setIsLoader(false);
-    // }
     let url;
     const classUrl = `${localStorage.getItem("baseUrl")}classes/${folderCls}`;
 
     if (folderPtr) {
-      url = `${classUrl}?where={"Folder": {"__type":"Pointer","className":${folderCls},"objectId":"${folderPtr.objectId}"},"Type":"Folder"}`;
+      url = `${classUrl}?where={"Folder": {"__type":"Pointer","className":"${folderCls}","objectId":"${folderPtr.objectId}"},"Type":"Folder"}`;
     } else {
       url = `${classUrl}?where={"Folder":{"$exists":false},"Type":"Folder"}`;
     }
@@ -160,7 +136,8 @@ const SelectFolder = ({
   const handleCreate = () => {
     setIsAdd(!isAdd);
   };
-  const handleAddFolder = () => {
+  const handleAddFolder = (res) => {
+    setPdfData((prev) => [...prev, res]);
     setFolderList([]);
     if (clickFolder && clickFolder.ObjectId) {
       fetchFolder({
@@ -174,39 +151,7 @@ const SelectFolder = ({
   };
 
   return (
-    <div className="text-xs mt-2">
-      {/* <div>
-        <label className="block">
-          Select Folder
-          {required && <span style={{ color: "red", fontSize: 13 }}> *</span>}
-        </label>
-      </div>
-      <div className="rounded px-[20px] py-[20px] bg-white border border-gray-200 shadow flex max-w-sm gap-8 items-center">
-        <div>
-          <i
-            className="far fa-folder-open text-[40px] text-[#33bbff]"
-            style={{ fontSize: "40px" }}
-            aria-hidden="true"
-          ></i>
-        </div>
-        <div className="font-semibold ">
-          <div className="flex items-center gap-2">
-            <p>
-              {selectFolder && selectFolder.Name ? selectFolder.Name : "Root"}
-            </p>
-            <div className="text-black text-sm" onClick={() => SetIsOpen(true)}>
-              <i
-                className="fa fa-pencil"
-                title="Select Folder"
-                aria-hidden="true"
-              ></i>
-            </div>
-          </div>
-          <p className="text-[10px] text-gray-400">
-            {selectFolder && selectFolder.Name ? `(${folderPath})` : ""}
-          </p>
-        </div>
-      </div> */}
+    <div>
       <ModalUi
         id="asd"
         title={"Select Folder"}
@@ -309,7 +254,7 @@ const SelectFolder = ({
             display: "flex",
             justifyContent: "space-between",
             alignItems: "center",
-            padding: "0 16px"
+            padding: "0px 16px 12px 16px"
           }}
         >
           <div
@@ -332,101 +277,6 @@ const SelectFolder = ({
           </div>
         </div>
       </ModalUi>
-      {/* {isOpen && (
-        <div
-          className={`fixed z-40 top-20 left-1/2 transform -translate-x-1/2 border-[1px] text-sm bg-white rounded `}
-        >
-          <div className="flex justify-between items-center py-[.75rem] px-[1.25rem] bg-[#f5f5f5]">
-            <div className="font-semibold text-lg text-black">
-              Select Folder
-            </div>
-            <div
-              onClick={handleCancel}
-              className="px-2 py-1 border-[1px] border-[#8a8a8a] bg-white rounded cursor-pointer"
-            >
-              <i className="fa-solid fa-xmark"></i>
-            </div>
-          </div>
-          <hr />
-          <div className="w-full min-w-[300px] md:min-w-[500px] px-3">
-            <div className="py-2 text-[#ac4848] text-[14px] font-[500]">
-              <span
-                className="cursor-pointer"
-                title="Root"
-                onClick={(e) => removeTabListItem(e)}
-              >
-                Root /{" "}
-              </span>
-              {tabList &&
-                tabList.map((tab, i) => (
-                  <React.Fragment key={`${tab.objectId}-${i}`}>
-                    <span
-                      className="cursor-pointer"
-                      title={tab.Name}
-                      onClick={(e) => removeTabListItem(e, i)}
-                    >
-                      {tab.Name}
-                    </span>
-                    {" / "}
-                  </React.Fragment>
-                ))}
-              <hr />
-            </div>
-            <div className="mt-2 mb-3">
-              {!isAdd &&
-                folderList.length > 0 &&
-                folderList.map((folder) => (
-                  <div
-                    key={folder.Name}
-                    className="border-[1px] border-[#8a8a8a] px-2 py-2 mb-2 cursor-pointer"
-                    onClick={() => handleSelect(folder)}
-                  >
-                    <div className="flex items-center gap-2">
-                      <i
-                        className="fa fa-folder text-[#33bbff] text-[1.4rem]"
-                        aria-hidden="true"
-                      ></i>
-                      <span className="font-semibold">{folder.Name}</span>
-                    </div>
-                  </div>
-                ))}
-              {isAdd && (
-                <CreateFolder
-                  parentFolderId={clickFolder && clickFolder.ObjectId}
-                  folderCls={folderCls}
-                  onSuccess={handleAddFolder}
-                />
-              )}
-              {isLoader && (
-                <div className="flex justify-center">
-                  <i className="fa-solid fa-spinner fa-spin-pulse text-[30px]"></i>
-                </div>
-              )}
-            </div>
-          </div>
-          <hr />
-          <div className="flex justify-between items-center py-[.75rem] px-[1.25rem]">
-            <div
-              className="text-[30px] cursor-pointer text-[#33bbff]"
-              title="Save Here"
-              onClick={handleCreate}
-            >
-              {isAdd ? (
-                <i className="fa-solid fa-arrow-left" aria-hidden="true"></i>
-              ) : (
-                <i className="fa-solid fa-square-plus" aria-hidden="true"></i>
-              )}
-            </div>
-            <div
-              className="text-[30px] cursor-pointer"
-              title="Save Here"
-              onClick={handleSubmit}
-            >
-              <i className="fas fa-save" aria-hidden="true"></i>
-            </div>
-          </div>
-        </div>
-      )} */}
     </div>
   );
 };
