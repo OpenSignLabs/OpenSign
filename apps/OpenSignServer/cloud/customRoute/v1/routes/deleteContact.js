a5fe4a345b1bf2e9abff4d5d22d6c814689cc168
prafull-opensignlabs <prafull.navkar@nxglabs.com>
2024-01-12 20:31:04 +0530

    update response message in delete contact api

--- apps/OpenSignServer/cloud/customRoute/v1/routes/deleteContact.js
+++ apps/OpenSignServer/cloud/customRoute/v1/routes/deleteContact.js
@@ -26,11 +26,11 @@ export default async function deleteContact(request, response) {
           deleteQuery.set('IsDeleted', true);
           const deleteRes = await deleteQuery.save(null, { useMasterKey: true });
           if (deleteRes) {
-            return response.json({ code: 200, message: 'Record delete successfully!' });
+            return response.json({ code: 200, message: 'Contact deleted successfully!' });
           }
         }
       } else {
-        return response.json({ code: 404, message: 'Record not found!' });
+        return response.json({ code: 404, message: 'Contact not found!' });
       }
     } else {
       return response.json({ code: 405, message: 'Invalid API Token!' });
