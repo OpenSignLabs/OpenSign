b4bd9f04471104dbc20304b9e68dd5b1a9ab1a51
prafull-opensignlabs <prafull.navkar@nxglabs.com>
2024-01-17 16:33:59 +0530

    fix: create contact not working and handle err

--- apps/OpenSignServer/cloud/customRoute/v1/routes/getContactList.js
+++ apps/OpenSignServer/cloud/customRoute/v1/routes/getContactList.js
@@ -38,6 +38,6 @@ export default async function getContactList(request, response) {
     }
   } catch (err) {
     console.log('err ', err);
-    return response.json(err);
+    return response.status(400).json({ error: 'Something went wrong, please try again later!' });
   }
 }
