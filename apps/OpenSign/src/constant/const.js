52320e0cd9054fa9755e1ed9d3bf53aba1338c96
prafull-opensignlabs <prafull.navkar@nxglabs.com>
2024-01-22 15:35:38 +0530

    fix: add regenerate confirmation modal in API token menu

--- apps/OpenSign/src/constant/const.js
+++ apps/OpenSign/src/constant/const.js
@@ -1,2 +1,6 @@
 export const contactCls = "contracts_Contactbook";
 export const templateCls = "contracts_Template";
+export const rejectBtn =
+  "bg-[#ffffff] rounded-sm shadow-md text-[12px] font-semibold uppercase text-black py-1.5 px-4 focus:outline-none text-center border-[1px] border-[#b4b4b4]";
+export const submitBtn =
+  "bg-[#32a3ac] rounded-sm shadow-md text-[12px] font-semibold uppercase text-white py-1.5 px-4 focus:outline-none text-center";
