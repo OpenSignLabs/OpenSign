5d2f89098f1f7e9e6d648ba02fa47d9d950a0586
prafull-opensignlabs <prafull.navkar@nxglabs.com>
2024-02-01 20:26:36 +0530

    feat: create document and template with coordinate

--- apps/OpenSignServer/Utils.js
+++ apps/OpenSignServer/Utils.js
@@ -2,3 +2,18 @@ export function customAPIurl() {
   const url = new URL(process.env.SERVER_URL);
   return url.pathname === '/api/app' ? url.origin + '/api' : url.origin;
 }
+
+export const color = [
+  '#93a3db',
+  '#e6c3db',
+  '#c0e3bc',
+  '#bce3db',
+  '#b8ccdb',
+  '#ceb8db',
+  '#ffccff',
+  '#99ffcc',
+  '#cc99ff',
+  '#ffcc99',
+  '#66ccff',
+  '#ffffcc',
+];
