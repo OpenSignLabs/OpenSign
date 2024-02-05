da846dcbbb8d67050992045924b03c2733d3ad58
prafull-opensignlabs <prafull.navkar@nxglabs.com>
2024-02-05 18:26:22 +0530

    fix: issue of request payload entity too large

--- apps/OpenSignServer/index.js
+++ apps/OpenSignServer/index.js
@@ -131,7 +131,8 @@ export const config = {
 
 export const app = express();
 app.use(cors());
-
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(function (req, res, next) {
   // console.log("req ", req.headers);
   // console.log("x-forwarded-for", req.headers["x-forwarded-for"]);
