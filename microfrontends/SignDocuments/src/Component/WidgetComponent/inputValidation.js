4239e8d5fb68601dd835dca93de17245da3098de
RaktimaNXG <raktima.c@nxglabs.com>
2024-02-29 17:58:32 +0530

    fix: import useEffect issue

--- microfrontends/SignDocuments/src/Component/WidgetComponent/inputValidation.js
+++ microfrontends/SignDocuments/src/Component/WidgetComponent/inputValidation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { themeColor } from "../../utils/ThemeColor/backColor";
 import ModalUi from "../../premitives/ModalUi";
 
