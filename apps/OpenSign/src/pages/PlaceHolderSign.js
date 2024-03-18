2989565d54ad9b0101fa0455c7b88040f8f9575f
RaktimaNXG <raktima.c@nxglabs.com>
2024-03-18 17:40:27 +0530

    fix: show warning on placeholder_sign page for document already send to signers for signature

--- apps/OpenSign/src/pages/PlaceHolderSign.js
+++ apps/OpenSign/src/pages/PlaceHolderSign.js
@@ -102,6 +102,10 @@ function PlaceHolderSign() {
   const [widgetName, setWidgetName] = useState(false);
   const [mailStatus, setMailStatus] = useState("");
   const [isCurrUser, setIsCurrUser] = useState(false);
+  const [isAlreadyPlace, setIsAlreadyPlace] = useState({
+    status: false,
+    message: ""
+  });
   const color = [
     "#93a3db",
     "#e6c3db",
@@ -193,15 +197,39 @@ function PlaceHolderSign() {
     //getting document details
     const documentData = await contractDocument(documentId);
     if (documentData && documentData.length > 0) {
-      // const alreadyPlaceholder =
-      //   documentData[0].Placeholders && documentData[0].Placeholders;
-      // if (alreadyPlaceholder && alreadyPlaceholder.length > 0) {
-      //   setIsAlreadyPlace(true);
-      // }
-      // setSignersData(documentData[0]);
-      // setIsSelectId(0);
-      // setSignerObjId(documentData[0].Signers[0].objectId);
-      // setContractName(documentData[0].Signers[0].className);
+      const alreadyPlaceholder = documentData[0]?.SignedUrl;
+
+      if (alreadyPlaceholder) {
+        const isCompleted =
+          documentData[0].IsCompleted && documentData[0]?.IsCompleted;
+        const expireDate = documentData[0].ExpiryDate.iso;
+        const declined =
+          documentData[0].IsDeclined && documentData[0]?.IsDeclined;
+        const expireUpdateDate = new Date(expireDate).getTime();
+        const currDate = new Date().getTime();
+        if (isCompleted) {
+          setIsAlreadyPlace({
+            status: true,
+            message: "This document has been signed by all Signers."
+          });
+        } else if (declined) {
+          setIsAlreadyPlace({
+            status: true,
+            message: "This document has been declined by one or more person(s)."
+          });
+        } else if (currDate > expireUpdateDate) {
+          setIsAlreadyPlace({
+            status: true,
+            message: "This Document is no longer available."
+          });
+        } else {
+          setIsAlreadyPlace({
+            status: true,
+            message: "The document has already been dispatched to be signed."
+          });
+        }
+      }
+
       setPdfDetails(documentData);
 
       if (documentData[0].Signers && documentData[0].Signers.length > 0) {
@@ -333,6 +361,7 @@ function PlaceHolderSign() {
       let dropData = [];
       let placeHolder;
       const dragTypeValue = item?.text ? item.text : monitor.type;
+      console.log("option", dragTypeValue);
       if (item === "onclick") {
         const dropObj = {
           //onclick put placeholder center on pdf
@@ -1749,6 +1778,36 @@ function PlaceHolderSign() {
               className="finishBtn cancelBtn"
             >
               Close
+            </button>
+          </div>
+        </ModalUi>
+        <ModalUi
+          headerColor={"#dc3545"}
+          isOpen={isAlreadyPlace.status}
+          title={"Document Alert"}
+          showClose={false}
+          // handleClose={() => {
+          //   setIsAlreadyPlace({status:false});
+          // }}
+        >
+          <div style={{ height: "100%", padding: 20 }}>
+            <p>{isAlreadyPlace.message}</p>
+
+            <div
+              style={{
+                height: "1px",
+                backgroundColor: "#9f9f9f",
+                width: "100%",
+                marginTop: "15px",
+                marginBottom: "15px"
+              }}
+            ></div>
+            <button
+              onClick={() => handleRecipientSign()}
+              type="button"
+              className="finishBtn cancelBtn"
+            >
+              View
             </button>
           </div>
         </ModalUi>
