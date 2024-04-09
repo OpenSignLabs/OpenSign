0bb857a94295e09c0ccd2827b4e31b98f1b17fd4
prafull-opensignlabs <prafull.navkar@nxglabs.com>
2024-04-09 17:37:43 +0530

    chore: update url

--- apps/OpenSign/src/json/plansArr.js
+++ apps/OpenSign/src/json/plansArr.js
@@ -1,14 +1,14 @@
-const plans= [
+const plans = [
   {
-    "planName": "OPENSIGN™ FREE",
-    "currency": "",
-    "monthlyPrice": "Free",
-    "yearlyPrice": "Free",
-    "subtitle": "Free Unlimited E-signatures, Forever.",
-    "btnText": "Subscribe",
-    "url": "",
-    "target": "_blank",
-    "benefits": [
+    planName: "OPENSIGN™ FREE",
+    currency: "",
+    monthlyPrice: "Free",
+    yearlyPrice: "Free",
+    subtitle: "Free Unlimited E-signatures, Forever.",
+    btnText: "Subscribe",
+    url: "",
+    target: "_blank",
+    benefits: [
       "Unlimited envelopes",
       "Sign documents yourself",
       "Request signatures from others",
@@ -27,16 +27,22 @@ const plans= [
     ]
   },
   {
-    "planName": "OPENSIGN™ PROFESSIONAL",
-    "currency": "$",
-    "monthlyPrice": "29.99",
-    "yearlyPrice": "329.99",
-    "subtitle": "Exclusive Access to advanced features.",
-    "btnText": "Subscribe",
-    "url": "https://billing.opensignlabs.com/subscribe/ef798486e6a0a11ea65f2bae8f2af901d1a09dfa8085585cdd4ec4d7f32137f3/professional-monthly",
-    "yearlyUrl": "https://billing.opensignlabs.com/subscribe/ef798486e6a0a11ea65f2bae8f2af901d8ad1135190dff951330360e47585a71/professional-yearly",
-    "target": "_self",
-    "benefits": [
+    planName: "OPENSIGN™ PROFESSIONAL",
+    currency: "$",
+    monthlyPrice: "29.99",
+    yearlyPrice: "329.99",
+    subtitle: "Exclusive Access to advanced features.",
+    btnText: "Subscribe",
+    url:
+      window.location.origin === "https://staging-app.opensignlabs.com"
+        ? "https://billing.zoho.in/subscribe/9627f62a09df1c8ce500f2f4dc8328dd84ecda20eeae13878ce581d60240f206/pro-weekly"
+        : "https://billing.opensignlabs.com/subscribe/ef798486e6a0a11ea65f2bae8f2af901d1a09dfa8085585cdd4ec4d7f32137f3/professional-monthly",
+    yearlyUrl:
+      window.location.origin === "https://staging-app.opensignlabs.com"
+        ? "https://billing.zoho.in/subscribe/9627f62a09df1c8ce500f2f4dc8328ddc184411129224f1f29ed84f6cef3b862/pro-yearly"
+        : "https://billing.opensignlabs.com/subscribe/ef798486e6a0a11ea65f2bae8f2af901d8ad1135190dff951330360e47585a71/professional-yearly",
+    target: "_self",
+    benefits: [
       "Everything in OpenSign™ professional",
       "Field validations",
       "Regular expression validations",
@@ -50,15 +56,15 @@ const plans= [
     ]
   },
   {
-    "planName": "OPENSIGN™ ENTERPRISE",
-    "currency": "",
-    "monthlyPrice": "Request Price",
-    "yearlyPrice": "Request Price",
-    "subtitle": "Scalable Features with priority support.",
-    "btnText": "Contact us",
-    "url": "https://www.opensignlabs.com/contact-us",
-    "target": "_blank",
-    "benefits": [
+    planName: "OPENSIGN™ ENTERPRISE",
+    currency: "",
+    monthlyPrice: "Request Price",
+    yearlyPrice: "Request Price",
+    subtitle: "Scalable Features with priority support.",
+    btnText: "Contact us",
+    url: "https://www.opensignlabs.com/contact-us",
+    target: "_blank",
+    benefits: [
       "All features",
       "Custom domain",
       "Custom branding",
@@ -67,6 +73,6 @@ const plans= [
       "Priority support"
     ]
   }
-]
+];
 
-export default plans
+export default plans;
