a668aa11db615ca7c90c695d5426205d8ce02a24
Amol <amol@nxglabs.in>
2024-04-14 16:20:24 +0530

    Update home page

--- docs/src/components/HomepageFeatures/index.js
+++ docs/src/components/HomepageFeatures/index.js
@@ -10,7 +10,7 @@ const FeatureList = [
     PNGUrl: '/img/logo.svg',
     description: (
       <>
-        <div style={{ textAlign: 'justify' }}>At OpenSign Labs, we're redefining the landscape of electronic signatures. Our mission is to make document signing not just a formality, but an integral part of your digital transformation journey. OpenSign Docs is your gateway to a world where efficiency, security, and simplicity reign supreme.</div>
+        <div style={{ textAlign: 'justify' }}>At OpenSign Labs, we're redefining the landscape of digital signatures. Our mission is to make digital document signing not just a formality, but an integral part of your organization's digital transformation journey. OpenSign™ Docs is your gateway to a world where efficiency, security and simplicity reign supreme.</div>
       </>
     ),
   },
@@ -25,13 +25,13 @@ const FeatureList = [
             <strong>Innovative Technology</strong>: Harness the latest advancements in e-signature technology.
         </li>
         <li>
-            <strong>Data-Driven Security</strong>: With encryption and compliance at the forefront, your documents are in safe hands.
+            <strong>Data-Driven Security</strong>: With encryption and compliance at the forefront, your documents are in safe hands. We also provide self host option for those who want full control of their data.
         </li>
         <li>
-            <strong>User-Centric Design</strong>: Experience unparalleled ease of use, designed for individuals and businesses alike.
+            <strong>User-Centric Design</strong>: Experience unparalleled ease of use, designed for individuals and businesses alike. OpenSign™ user experience is designed while keeping the most novice user in mind. Finally, a tool that does not need numerous hours of training to get started.
         </li>
         <li>
-            <strong>Scalable Solutions</strong>: From small startups to large corporations, our platform grows with your needs.
+            <strong>Scalable Solutions</strong>: From small startups to large corporations, our platform grows with your needs. From free for life plan for individuals to branding and single-sign-on support for enterprises, our platform supports everything.
         </li>
     </ul>
 </div>
@@ -45,17 +45,17 @@ const FeatureList = [
       <>
         <div style={{ textAlign: 'justify' }}>
     <p>
-        Dive into our comprehensive guides and tutorials. Whether you're a new user or looking to integrate OpenSign into your existing workflow, we have everything you need to get started.
+        Dive into our comprehensive guides and tutorials. Whether you're a new user or looking to integrate OpenSign™ into your existing workflow, we have everything you need to get started.
     </p>
     <ul>
         <li>
-            <strong>Tutorial</strong>: Get up and running in no time. Get all the help you need to navigate you through the already seamless UX of OpenSign.
+            <strong>[Tutorial](https://docs.opensignlabs.com/docs/help/intro)</strong>: Get up and running in no time. Get all the help you need to navigate you through the already seamless UX of OpenSign.
         </li>
         <li>
-            <strong>Github Discussions</strong>: Discuss your ideas & issues with the community.
+            <strong>[Github Discussions](https://github.com/OpenSignLabs/OpenSign/discussions)</strong>: Discuss your ideas & issues with the community.
         </li>
         <li>
-            <strong>Social media</strong>: Join us on Discord, Twitter, LinkedIn & Facebook.
+            <strong>Social media</strong>: Join us on [Discord](https://discord.com/invite/xe9TDuyAyj), [Twitter](https://www.twitter.com/opensignhq), [LinkedIn](https://www.linkedin.com/company/opensign%E2%84%A2/) & [Facebook](https://www.facebook.com/profile.php?id=61551030403669).
         </li>
     </ul>
 </div>
