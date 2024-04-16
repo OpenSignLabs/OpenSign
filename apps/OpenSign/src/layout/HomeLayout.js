11384192baba02723bdea79bbfac16cce1cc2368
RaktimaNXG <raktima.c@nxglabs.com>
2024-04-16 12:08:50 +0530

    refactor code

--- apps/OpenSign/src/layout/HomeLayout.js
+++ apps/OpenSign/src/layout/HomeLayout.js
@@ -295,7 +295,7 @@ const HomeLayout = () => {
       ) : (
         <ModalUi title={"Session Expired"} isOpen={true} showClose={false}>
           <div className="flex flex-col justify-center items-center py-4 md:py-5 gap-5">
-            {/* <p className="text-xl font-normal">Your session has expired.</p> */}
+            <p className="text-xl font-normal">Your session has expired.</p>
             <button
               onClick={handleLoginBtn}
               className="text-base px-3 py-1.5 rounded shadow-md text-white bg-[#1ab6ce]"
