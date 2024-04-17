c5d4f753787397c91e42b28c906a6f76a3d40b4a
RaktimaNXG <raktima.c@nxglabs.com>
2024-04-17 14:30:53 +0530

    fix: opensign-drive tour message error andtemplate report tour message

--- apps/OpenSign/src/primitives/GetReportDisplay.js
+++ apps/OpenSign/src/primitives/GetReportDisplay.js
@@ -1,4 +1,4 @@
- import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import pad from "../assets/images/pad.svg";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -8,6 +8,8 @@ import { modalSubmitBtnColor, modalCancelBtnColor } from "../constant/const";
 import Alert from "./Alert";
 import Tooltip from "./Tooltip";
 import { RWebShare } from "react-web-share";
+import Tour from "reactour";
+import Parse from "parse";
 
 const ReportTable = ({
   ReportName,
@@ -19,7 +21,9 @@ const ReportTable = ({
   isMoreDocs,
   docPerPage,
   form,
-  report_help
+  report_help,
+  tourData,
+  isDontShow
 }) => {
   const navigate = useNavigate();
   const [currentPage, setCurrentPage] = useState(1);
@@ -33,6 +37,8 @@ const ReportTable = ({
   const [shareUrls, setShareUrls] = useState([]);
   const [copied, setCopied] = useState(false);
   const [alertMsg, setAlertMsg] = useState({ type: "success", message: "" });
+  const [isTour, setIsTour] = useState(false);
+  const [tourStatusArr, setTourStatusArr] = useState([]);
   const startIndex = (currentPage - 1) * docPerPage;
 
   // For loop is used to calculate page numbers visible below table
@@ -46,6 +52,7 @@ const ReportTable = ({
   }, [List, docPerPage]);
   //  below useEffect reset currenpage to 1 if user change route
   useEffect(() => {
+    checkTourStatus();
     return () => setCurrentPage(1);
   }, []);
 
@@ -350,6 +357,80 @@ const ReportTable = ({
         setActLoader({});
       });
   };
+
+  async function checkTourStatus() {
+    const currentUser = Parse.User.current();
+    const cloudRes = await Parse.Cloud.run("getUserDetails", {
+      email: currentUser.get("email")
+    });
+    const res = { data: cloudRes.toJSON() };
+    if (res.data && res.data.TourStatus && res.data.TourStatus.length > 0) {
+      const tourStatus = res.data.TourStatus;
+      // console.log("res ", res.data.TourStatus);
+      setTourStatusArr(tourStatus);
+      const filteredtourStatus = tourStatus.filter(
+        (obj) => obj["templateReportTour"]
+      );
+      if (filteredtourStatus.length > 0) {
+        const templateReportTour = filteredtourStatus[0]["templateReportTour"];
+
+        if (templateReportTour) {
+          setIsTour(false);
+        } else {
+          setIsTour(true);
+        }
+      } else {
+        setIsTour(true);
+      }
+    } else {
+      setIsTour(true);
+    }
+  }
+
+  const closeTour = async () => {
+    // console.log("closeTour");
+    setIsTour(false);
+    if (isDontShow) {
+      const serverUrl = localStorage.getItem("baseUrl");
+      const appId = localStorage.getItem("parseAppId");
+      const extUserClass = localStorage.getItem("extended_class");
+      const json = JSON.parse(localStorage.getItem("Extand_Class"));
+      const extUserId = json && json.length > 0 && json[0].objectId;
+      // console.log("extUserId ", extUserId)
+
+      let updatedTourStatus = [];
+      if (tourStatusArr.length > 0) {
+        updatedTourStatus = [...tourStatusArr];
+        const templateReportTourIndex = tourStatusArr.findIndex(
+          (obj) =>
+            obj["templateReportTour"] === false ||
+            obj["templateReportTour"] === true
+        );
+        if (templateReportTourIndex !== -1) {
+          updatedTourStatus[templateReportTourIndex] = {
+            templateReportTour: true
+          };
+        } else {
+          updatedTourStatus.push({ templateReportTour: true });
+        }
+      } else {
+        updatedTourStatus = [{ templateReportTour: true }];
+      }
+
+      await axios.put(
+        serverUrl + "classes/" + extUserClass + "/" + extUserId,
+        {
+          TourStatus: updatedTourStatus
+        },
+        {
+          headers: {
+            "X-Parse-Application-Id": appId
+          }
+        }
+      );
+    }
+  };
+
   return (
     <div className="relative">
       {Object.keys(actLoader)?.length > 0 && (
@@ -362,6 +443,15 @@ const ReportTable = ({
       )}
       <div className="p-2 overflow-x-scroll w-full bg-white rounded-md">
         {isAlert && <Alert type={alertMsg.type}>{alertMsg.message}</Alert>}
+        {tourData && ReportName === "Templates" && (
+          <Tour
+            onRequestClose={closeTour}
+            steps={tourData}
+            isOpen={isTour}
+            // rounded={5}
+            closeWithMask={false}
+          />
+        )}
         <div className="flex flex-row items-center justify-between my-2 mx-3 text-[20px] md:text-[23px]">
           <div className="font-light">
             {ReportName}{" "}
@@ -373,6 +463,7 @@ const ReportTable = ({
           </div>
           {ReportName === "Templates" && (
             <i
+              data-tut="reactourFirst"
               onClick={() => navigate("/form/template")}
               className="fa-solid fa-square-plus text-sky-400 text-[25px]"
             ></i>
@@ -504,6 +595,7 @@ const ReportTable = ({
                         {actions?.length > 0 &&
                           actions.map((act, index) => (
                             <button
+                              data-tut={act?.selector}
                               key={index}
                               onClick={() => handleActionBtn(act, item)}
                               className={`w-[25px] h-[25px] flex justify-center items-center rounded shadow`}
