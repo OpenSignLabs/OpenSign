4cbd7e74f783a759d95610f89b7694ac6e8a11bd
RaktimaNXG <raktima.c@nxglabs.com>
2024-04-17 21:02:05 +0530

    fix: onclick upgrade to customize email button redirect new window of subscription page

--- apps/OpenSign/src/primitives/Upgrade.js
+++ apps/OpenSign/src/primitives/Upgrade.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { openInNewTab } from "../constant/Utils";
 
-function Upgrade({ message }) {
+function Upgrade({ message, newWindow }) {
   const navigation = useNavigate();
 
   return (
     <sup>
       <span
-        onClick={() => navigation("/subscription")}
+        onClick={() => {
+          if (newWindow) {
+            const url = window.location.origin + "/subscription";
+            openInNewTab(url);
+          } else {
+            navigation("/subscription");
+          }
+        }}
         className="text-blue-800 text-sm cursor-pointer underline"
       >
         {message ? message : "Upgrade now"}
