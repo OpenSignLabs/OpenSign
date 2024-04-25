14ce91350ebab830d46abbf8203ae02dc79a5fc4
prafull-opensignlabs <prafull.navkar@nxglabs.com>
2024-04-25 12:16:45 +0530

    fix: missing credintials in config aws_config_file

--- apps/OpenSignServer/cloud/parsefunction/TenantAfterFind.js
+++ apps/OpenSignServer/cloud/parsefunction/TenantAfterFind.js
@@ -1,14 +1,17 @@
+import { useLocal } from '../../Utils.js';
 import getPresignedUrl from './getSignedUrl.js';
 
 async function TenantAterFind(request) {
-  if (request.objects.length === 1) {
-    if (request.objects) {
-      const obj = request.objects[0];
-      const Logo = obj?.get('Logo') && obj?.get('Logo');
-      if (Logo) {
-        obj.set('Logo', getPresignedUrl(Logo));
+  if (useLocal !== 'true') {
+    if (request.objects.length === 1) {
+      if (request.objects) {
+        const obj = request.objects[0];
+        const Logo = obj?.get('Logo') && obj?.get('Logo');
+        if (Logo) {
+          obj.set('Logo', getPresignedUrl(Logo));
+        }
+        return [obj];
       }
-      return [obj];
     }
   }
 }
