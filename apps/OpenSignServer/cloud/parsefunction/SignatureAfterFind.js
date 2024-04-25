5f68c0dd780294f1a74aa38b6e3a40cbf68aedc0
prafull-opensignlabs <prafull.navkar@nxglabs.com>
2024-04-25 12:16:45 +0530

    fix: missing credintials in config aws_config_file

--- apps/OpenSignServer/cloud/parsefunction/SignatureAfterFind.js
+++ apps/OpenSignServer/cloud/parsefunction/SignatureAfterFind.js
@@ -1,21 +1,21 @@
+import { useLocal } from '../../Utils.js';
 import getPresignedUrl from './getSignedUrl.js';
 
 async function SignatureAfterFind(request) {
-  if (request.objects.length === 1) {
-    if (request.objects) {
-      const obj = request.objects[0];
-      const ImageURL = obj?.get('ImageURL') && obj?.get('ImageURL');
-      const Initials = obj?.get('Initials') && obj?.get('Initials');
-
-      if (ImageURL) {
-        obj.set('ImageURL', getPresignedUrl(ImageURL));
+  if (useLocal !== 'true') {
+    if (request.objects.length === 1) {
+      if (request.objects) {
+        const obj = request.objects[0];
+        const ImageURL = obj?.get('ImageURL') && obj?.get('ImageURL');
+        const Initials = obj?.get('Initials') && obj?.get('Initials');
+        if (ImageURL) {
+          obj.set('ImageURL', getPresignedUrl(ImageURL));
+        }
+        if (Initials) {
+          obj.set('Initials', getPresignedUrl(Initials));
+        }
+        return [obj];
       }
-
-      if (Initials) {
-        obj.set('Initials', getPresignedUrl(Initials));
-      }
-
-      return [obj];
     }
   }
 }
