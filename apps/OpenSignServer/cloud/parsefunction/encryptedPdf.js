4d883a0798fb7ed045a239682f81904e8cbd622a
prafull-opensignlabs <prafull.navkar@nxglabs.com>
2024-05-03 14:26:54 +0530

    fix: issue of encryptedpdf error api not working

--- apps/OpenSignServer/cloud/parsefunction/encryptedPdf.js
+++ apps/OpenSignServer/cloud/parsefunction/encryptedPdf.js
@@ -1,6 +1,6 @@
 import { PostHog } from 'posthog-node';
 const ph_project_api_key = process.env.PH_PROJECT_API_KEY;
-const client = new PostHog(ph_project_api_key);
+const client = ph_project_api_key ? new PostHog(ph_project_api_key) : '';
 export default async function encryptedpdf(request) {
   const email = request.params.email;
   if (client) {
