afb4de9d0c3d78ce6dfe5d6fced2d2b2ed7849f5
prafull-opensignlabs <prafull.navkar@nxglabs.com>
2024-05-31 18:22:16 +0530

    Feat: create SSO user account on platform

--- apps/OpenSignServer/cloud/parsefunction/ssoSignin.js
+++ apps/OpenSignServer/cloud/parsefunction/ssoSignin.js
@@ -3,39 +3,37 @@ import axios from 'axios';
 const serverUrl = process.env.SERVER_URL;
 const APPID = process.env.APP_ID;
 const masterKEY = process.env.MASTER_KEY;
-
+const clientUrl = process.env.PUBLIC_URL;
+const ssoApiUrl = process.env.SSO_API_URL || 'https://osl-jacksonv2.vercel.app/api';
 /**
  * ssoSign is function which is used to sign up/sign in with SSO
  * @param code It is code return by jackson using authorize endpoint
  * @param email It is user's email with user sign in/sign up
- * @returns if success {email, message, sessiontoken} else on reject {message}
+ * @returns if success {email, name, phone message, sessiontoken} else on reject error {code, message}
  */
 
 export default async function ssoSignin(request) {
   const code = request.params.code;
   const userEmail = request.params.email;
-
-  //   console.log('code ', code);
   try {
     const headers = { 'content-type': 'application/x-www-form-urlencoded' };
     const axiosRes = await axios.post(
-      'https://osl-jacksonv2.vercel.app/api/oauth/token',
+      ssoApiUrl + '/oauth/token',
       {
         grant_type: 'authorization_code',
         client_id: 'dummy',
         tenant: 'Okta-dev-nxglabs-in',
         product: 'OpenSign',
         client_secret: 'dummy',
-        redirect_uri: 'http://localhost:3000/sso',
+        redirect_uri: clientUrl + '/sso',
         code: code,
       },
       { headers: headers }
     );
     const ssoAccessToken = axiosRes.data && axiosRes.data.access_token;
-    // console.log('ssoAccessToken ', ssoAccessToken);
     const authData = { sso: { id: userEmail, access_token: ssoAccessToken } };
     const userQuery = new Parse.Query(Parse.User);
-    userQuery.equalTo('email', userEmail);
+    userQuery.equalTo('username', userEmail);
     const res = await userQuery.first({ useMasterKey: true });
     if (res) {
       try {
@@ -51,17 +49,31 @@ export default async function ssoSignin(request) {
         );
 
         if (SignIn.data) {
+          const response = await axios.get(ssoApiUrl + '/oauth/userinfo', {
+            headers: {
+              Authorization: `Bearer ${ssoAccessToken}`,
+            },
+          });
           const sessiontoken = SignIn.data.sessionToken;
           //   console.log('sso sessiontoken', sessiontoken);
-          return {
+          const payload = {
             email: userEmail,
+            name: response.data?.firstName + ' ' + response.data?.lastName,
+            phone: response?.data?.phone || '',
             message: 'User Sign In',
             sessiontoken: sessiontoken,
           };
+          return payload;
         }
       } catch (err) {
-        console.log('err in user sso sign in', err);
-        throw new Parse.Error(Parse.Error.INTERNAL_SERVER_ERROR, 'Internal server error.');
+        const errCode = err?.response?.data?.code || err?.response?.status || err?.code || 400;
+        const message =
+          err?.response?.data?.error ||
+          err?.response?.data ||
+          err?.message ||
+          'Internal server error.';
+        console.log('err in user sso sign in', errCode, message);
+        throw new Parse.Error(errCode, message);
       }
     } else {
       // console.log("in sign up condition");
@@ -79,7 +91,7 @@ export default async function ssoSignin(request) {
               username: response.data.email,
               email: response.data.email,
               phone: response.data?.phone,
-              name: response.data?.firstName + response.data?.lastName,
+              name: response.data?.firstName + ' ' + response.data?.lastName,
             },
             {
               headers: {
@@ -88,21 +100,26 @@ export default async function ssoSignin(request) {
               },
             }
           );
-
-          // console.log("SignUp", SignUp);
-
           if (SignUp.data) {
             const sessiontoken = SignUp.data.sessionToken;
             const payload = {
               email: userEmail,
+              name: SignUp?.data?.name,
+              phone: SignUp?.data?.phone || '',
               message: 'User Sign Up',
               sessiontoken: sessiontoken,
             };
             return payload;
           }
         } catch (err) {
-          console.log('err in user sso sign up', err);
-          throw new Parse.Error(Parse.Error.INTERNAL_SERVER_ERROR, 'Internal server error.');
+          const errCode = err?.response?.data?.code || err?.response?.status || err?.code || 400;
+          const message =
+            err?.response?.data?.error ||
+            err?.response?.data ||
+            err?.message ||
+            'Internal server error.';
+          console.log('err in user sso sign up', errCode, message);
+          throw new Parse.Error(errCode, message);
         }
       }
     }
