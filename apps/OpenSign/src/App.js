de120dae392e557e9426956672487b01beb2db27
prafull-opensignlabs <93375423+prafull-opensignlabs@users.noreply.github.com>
2024-06-16 00:03:48 +0530

    fix: update pdf worker import path

--- apps/OpenSign/src/App.js
+++ apps/OpenSign/src/App.js
@@ -31,10 +31,7 @@ const ManageSign = lazy(() => import("./pages/Managesign"));
 const GenerateToken = lazy(() => import("./pages/GenerateToken"));
 const Webhook = lazy(() => import("./pages/Webhook"));
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/legacy/build/pdf.worker.min.mjs",
-  import.meta.url
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.mjs`;
 const AppLoader = () => {
   return (
     <div className="flex justify-center items-center h-[100vh]">
