a43202e3dcfccfc6a54eff82f0e068dcc2830a0f
RaktimaNXG <raktima.c@nxglabs.com>
2024-08-12 18:02:32 +0530

    fix: multi-language support issue in public profile

--- apps/OpenSign/src/i18n.js
+++ apps/OpenSign/src/i18n.js
@@ -2,16 +2,6 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
-import { isPublicProduction, isPublicStaging } from "./constant/const";
-
-//handle path to support language on public profile
-const backendPath = isPublicStaging
-  ? "https://staging-app.opensignlabs.com/static/locales/{{lng}}/{{ns}}.json"
-  : isPublicProduction
-    ? "https://app.opensignlabs.com/static/locales/{{lng}}/{{ns}}.json"
-    : "/locales/{{lng}}/{{ns}}.json";
-
-//"http://localhost:3000/static/locales/{{lng}}/{{ns}}.json"
 
 i18n
   .use(Backend)
@@ -19,7 +9,7 @@ i18n
   .use(initReactI18next)
   .init({
     backend: {
-      loadPath: backendPath
+      loadPath: "/locales/{{lng}}/{{ns}}.json"
     },
     fallbackLng: "en", // Fallback to English if no other language is detected
     detection: {
