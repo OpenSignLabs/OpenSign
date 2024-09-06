d330e662d364ffba236e2ea605ea9c6b8f773ae7
prafull-opensignlabs <prafull.navkar@nxglabs.com>
2024-09-06 17:14:53 +0530

    refactor: correct keys in translation files

--- apps/OpenSign/src/primitives/QuotaCard.js
+++ apps/OpenSign/src/primitives/QuotaCard.js
@@ -15,10 +15,10 @@ const QuotaCard = ({ isPaidInfo, handlClose }) => {
       </div>
       <div className="op-card op-bg-primary text-primary-content w-full shadow-lg">
         <div className="op-card-body">
-          <h2 className="op-card-title">{t("quotamailinfohead")}</h2>
-          <p>{t("quotamailinfo")}</p>
+          <h2 className="op-card-title">{t("quota-mail-info-head")}</h2>
+          <p>{t("quota-mail-info")}</p>
           <p>
-            <Trans i18nKey={"quotamailTip"}>
+            <Trans i18nKey={"quota-mail-tip"}>
               Tip: You can still sign <strong>unlimited documents</strong> by
               manually sharing the signing request links.
             </Trans>
@@ -38,8 +38,8 @@ const QuotaCard = ({ isPaidInfo, handlClose }) => {
     <>
       <div className="op-card op-bg-primary text-primary-content w-full shadow-lg">
         <div className="op-card-body">
-          <p>{t("quotamail")}</p>
-          <p>{t("quotamailTip")}</p>
+          <p>{t("quota-mail")}</p>
+          <p>{t("quota-mail-tip")}</p>
           <div className="op-card-actions justify-end">
             <button
               onClick={() => navigate("/subscription")}
