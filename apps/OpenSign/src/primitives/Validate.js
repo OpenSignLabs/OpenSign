5d2e3ed4ee33e56fad0e676befad94bb567e0763
prafull-opensignlabs <93375423+prafull-opensignlabs@users.noreply.github.com>
2025-04-29 12:10:14 +0530

    Update Validate.js

--- apps/OpenSign/src/primitives/Validate.js
+++ apps/OpenSign/src/primitives/Validate.js
@@ -54,7 +54,7 @@ const Validate = () => {
   ) : (
     <ModalUi showHeader={false} isOpen={true} showClose={false}>
       <div className="flex flex-col justify-center items-center py-4 md:py-5 gap-5">
-        <p className="text-xl font-medium">{this("session-expired")}</p>
+        <p className="text-xl font-medium">{t("session-expired")}</p>
         <button onClick={handleLoginBtn} className="op-btn op-btn-neutral">
           {t("login")}
         </button>
