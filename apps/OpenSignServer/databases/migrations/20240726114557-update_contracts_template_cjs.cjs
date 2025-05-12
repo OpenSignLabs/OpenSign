a50f4d36073d1f517e6bbda241cb0a783bc48b52
Gaurav Katare <gauravkatare@Gauravs-MacBook-Air.local>
2025-05-12 13:30:23 +0530

    Migration_Tempelate_ErrorFix

--- apps/OpenSignServer/databases/migrations/20240726114557-update_contracts_template_cjs.cjs
+++ apps/OpenSignServer/databases/migrations/20240726114557-update_contracts_template_cjs.cjs
@@ -2,7 +2,7 @@
  *
  * @param {Parse} Parse
  */
-exports.up = async Parse => {
+const up = async Parse => {
   const className = 'contracts_Template';
   const schema = new Parse.Schema(className);
   schema.addString('OriginIp');
@@ -13,9 +13,11 @@ exports.up = async Parse => {
  *
  * @param {Parse} Parse
  */
-exports.down = async Parse => {
+const down = async Parse => {
   const className = 'contracts_Template';
   const schema = new Parse.Schema(className);
   schema.deleteField('OriginIp');
   return schema.update();
 };
+
+module.exports = { up, down };
